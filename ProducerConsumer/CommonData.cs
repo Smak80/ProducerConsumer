5e18a16e65fefaf9327037021d3d2467873b52fb
Smak80 <smaklets@kpfu.ru>
2021-04-01 15:31:14 +0300

    Создание класса общих данных и продюсера

--- ProducerConsumer/CommonData.cs
+++ ProducerConsumer/CommonData.cs
@@ -2,11 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProducerConsumer
 {
+    
     class CommonData
     {
+        public static readonly int ProducerCount = 3;
+
+        private Queue<int>[] values =
+        {
+            new Queue<int>(),
+            new Queue<int>(),
+            new Queue<int>()
+        };
+
+        public void AddValue(int index, int value)
+        {
+            index = Math.Abs(index) % ProducerCount;
+            Monitor.Enter(values);
+            values[index].Enqueue(value);
+            Monitor.PulseAll(values);
+            Monitor.Exit(values);
+        }
+
+        public int[] ConsumeValues()
+        {
+            var result = new int[ProducerCount];
+            int i = 0;
+            Monitor.Enter(values);
+            foreach (var queue in values)
+            {
+                
+                while (queue.Count == 0)
+                {
+                    Monitor.Wait(values);
+                }
+                result[i++] = queue.Dequeue();
+
+            }
+            Monitor.Exit(values);
+            return result;
+        }
     }
 }
---
+++ ProducerConsumer/Producer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumer
+{
+    class Producer
+    {
+        private CommonData Data
+        {
+            get;
+            set;
+        }
+
+        private int index;
+        public int Index
+        {
+            get => index;
+            set => index = Math.Abs(value) % CommonData.ProducerCount;
+        }
+
+        private Thread t = null;
+        private bool stop = false;
+        
+        private static Random r = new Random((int)DateTime.Now.Ticks);
+
+        public Producer(CommonData data, int index)
+        {
+            Data = data;
+            Index = index;
+        }
+
+        public void Start()
+        {
+            if (t?.IsAlive == true) return;
+            stop = false;
+            t = new Thread(new ThreadStart(produce));
+        }
+
+        private void produce()
+        {
+            while (!stop)
+            {
+                Thread.Sleep(r.Next(1000, 3000));
+                var result = r.Next(0, 100);
+                Data.AddValue(Index, result);
+            }
+        }
+
+        public void Stop()
+        {
+            stop = true;
+            t.Interrupt();
+        }
+    }
+}
